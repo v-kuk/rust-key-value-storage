05e78fa69a318e1e3d4d767951e6e366a3ee0386
Vladyslav Kukler <vladkukler@gmail.com>
2025-01-13 02:12:17 +0200

    refactor(server): improve code structure

--- src/server.rs
+++ src/server.rs
@@ -10,38 +10,38 @@ pub fn server_main(
 
     for received_raw in cmd_channel_receiver {
         let received = received_raw.as_str();
+        let args: Vec<&str> = received.split_whitespace().collect();
+        let command = args[0];
 
-        if received.starts_with("cmd:add") {
-            let args: Vec<&str> = received.split_whitespace().collect();
-            if args.len() >= 3 {
-                let key = args[1].to_string();
-                let value = args[2].to_string();
-
-                values.insert(key, value);
-                let _ = data_channel_sender.send("Value added!".to_string());
-            } else {
-                let _ = data_channel_sender.send(
-                    "Invalid cmd:add format. Use: cmd:add <key> <value>".to_string(),
-                );
-            }
-        } else {
-            match received {
-                "cmd:print" => {
-                    print!("Values: ");
-                    for (k, v) in &values {
-                        println!("{k}: {v}");
-                    }
-                    print!("");
-                },
-                "cmd:ping" => {
-                    let _ = data_channel_sender.send("server:pong".to_string());
-                },
-                "cmd:exit" => break,
-                _ => {
-                    let _ = data_channel_sender.send(format!("Unknown command: {}", received));
-                },
-                
-            }
+        match command {
+            "cmd:add" => {
+                if args.len() >= 3 {
+                    let key = args[1].to_string();
+                    let value = args[2].to_string();
+    
+                    values.insert(key, value);
+                    let _ = data_channel_sender.send("Value added!".to_string());
+                } else {
+                    let _ = data_channel_sender.send(
+                        "Invalid cmd:add format. Use: cmd:add <key> <value>".to_string(),
+                    );
+                }
+            },
+            "cmd:print" => {
+                print!("Values: ");
+                for (k, v) in &values {
+                    println!("{k}: {v}");
+                }
+                print!("");
+            },
+            "cmd:ping" => {
+                let _ = data_channel_sender.send("server:pong".to_string());
+            },
+            "cmd:exit" => break,
+            _ => {
+                let _ = data_channel_sender.send(format!("Unknown command: {}", received));
+            },
+            
         }
     }
 
